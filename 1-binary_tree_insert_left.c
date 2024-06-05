a75356f95883e39238a37d91cdeddf6dac887a32
jacklham <jackkuelmazierlham@gmail.com>
2024-06-05 01:25:44 -0700

    binary_tree_insert_left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,14 +8,17 @@
  * if parent already has a left child, replace with new node and set
  */
 
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value) {
-    if (parent == NULL) {
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+    if (parent == NULL)
+    {
         // Invalid parent pointer
         return NULL;
     }
 
     binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL) {
+    if (new_node == NULL)
+    {
         // Memory allocation failed
         return NULL;
     }
